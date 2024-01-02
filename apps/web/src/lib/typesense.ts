5b01be264f149940ecd05648b76445266cc90e1b
HM Ku <hmku@yakoo.com.hk>
2024-01-02 17:17:01 +0800

    update default model for openai

--- apps/web/src/lib/typesense.ts
+++ apps/web/src/lib/typesense.ts
@@ -27,7 +27,7 @@ function getEmbedding() {
         }
       ),
       llm: new OpenAI({
-        modelName: 'gpt-3.5-turbo',
+        modelName: 'gpt-3.5-turbo-1106',
         temperature: 0.8,
         maxTokens: 512,
         streaming: true,
