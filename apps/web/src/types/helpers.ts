601590d29c567d67e4f0159b05e58c1ab0c4e366
HM Ku <hmku@yakoo.com.hk>
2024-03-12 11:26:38 +0800

    add fixedForwardRef helper

--- apps/web/src/types/helpers.ts
+++ apps/web/src/types/helpers.ts
@@ -1,3 +1,5 @@
+import { forwardRef } from 'react';
+
 export type UnwrapArray<T> = T extends Array<infer U> ? U : T;
 
 // Reference: https://www.totaltypescript.com/concepts/the-prettify-helper
@@ -5,3 +7,11 @@ export type Prettify<T> = {
   [K in keyof T]: T[K];
   // eslint-disable-next-line @typescript-eslint/ban-types
 } & {};
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function fixedForwardRef<T, P = {}>(
+  render: (props: P, ref: React.Ref<T>) => React.ReactNode
+): (props: P & React.RefAttributes<T>) => React.ReactNode {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return forwardRef(render) as any;
+}
