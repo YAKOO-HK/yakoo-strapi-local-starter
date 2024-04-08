54df340e2a4f9d4f3d39b1393c7b1daf4ac25872
HM Ku <hmku@yakoo.com.hk>
2024-04-08 17:02:10 +0800

    fix server-sitemap.xml

--- apps/web/src/app/server-sitemap.xml/route.ts
+++ apps/web/src/app/server-sitemap.xml/route.ts
@@ -14,27 +14,31 @@ export async function GET() {
   return getServerSideSitemap([
     // pages: /:slug
     ...hundredPages.map(({ attributes }) => ({
-      loc: `${env.NEXT_PUBLIC_SITE_URL}/${attributes.locale}/${env.NEXT_PUBLIC_SITE_URL}/${attributes.slug}`,
+      loc: `${env.NEXT_PUBLIC_SITE_URL}/${attributes.locale}/${attributes.slug}`,
       lastmod: attributes.updatedAt,
       changefreq: 'weekly' as const,
+      priority: 0.7,
     })),
     // /posts
     ...locales.map((locale) => ({
       loc: `${env.NEXT_PUBLIC_SITE_URL}/${locale}/posts`,
       lastmod: now, // TODO: should be the latest post updatedAt/publishedAt?
       changefreq: 'daily' as const,
+      priority: 0.5,
     })),
     // /posts/:category
     ...categories.map(({ attributes }) => ({
       loc: `${env.NEXT_PUBLIC_SITE_URL}/${attributes.locale}/posts/${attributes.slug}`,
       lastmod: now, // TODO: should be the latest post updatedAt/publishedAt?
       changefreq: 'weekly' as const,
+      priority: 0.5,
     })),
     // /posts/:category/:slug
     ...hundredPosts.map(({ attributes }) => ({
       loc: `${env.NEXT_PUBLIC_SITE_URL}/${attributes.locale}/posts/${attributes.category?.data?.attributes.slug ?? '-'}/${attributes.slug}`,
       lastmod: attributes.updatedAt,
       changefreq: 'weekly' as const,
+      priority: 0.5,
     })),
   ]);
 }
