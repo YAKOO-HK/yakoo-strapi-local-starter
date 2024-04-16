bd280b3944a2af272369c204fbe9a69b50084947
HM Ku <hmku@yakoo.com.hk>
2024-04-16 14:14:44 +0800

    chore: Update dependencies

--- apps/web/src/navigation.ts
+++ apps/web/src/navigation.ts
@@ -1,4 +1,3 @@
-import { RedirectType, useRouter as useNextRouter } from 'next/navigation';
 import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 import { StrapiLocale } from './strapi/strapi';
 
@@ -6,68 +5,6 @@ export const locales: StrapiLocale[] = ['en', 'zh-Hant'];
 export const defaultLocale: StrapiLocale = 'en';
 export const localePrefix: 'as-needed' | 'always' | 'never' = 'always';
 
-const navigation: {
-  Link: (
-    // eslint-disable-next-line no-unused-vars
-    props: Omit<
-      Omit<
-        Omit<
-          Omit<
-            Omit<
-              React.AnchorHTMLAttributes<HTMLAnchorElement>,
-              keyof {
-                href: string | import('url').UrlObject;
-                as?: (string | import('url').UrlObject) | undefined;
-                replace?: boolean | undefined;
-                scroll?: boolean | undefined;
-                shallow?: boolean | undefined;
-                passHref?: boolean | undefined;
-                prefetch?: boolean | undefined;
-                locale?: string | false | undefined;
-                legacyBehavior?: boolean | undefined;
-                onMouseEnter?: React.MouseEventHandler<HTMLAnchorElement> | undefined;
-                onTouchStart?: React.TouchEventHandler<HTMLAnchorElement> | undefined;
-                onClick?: React.MouseEventHandler<HTMLAnchorElement> | undefined;
-              }
-            > & {
-              href: string | import('url').UrlObject;
-              as?: (string | import('url').UrlObject) | undefined;
-              replace?: boolean | undefined;
-              scroll?: boolean | undefined;
-              shallow?: boolean | undefined;
-              passHref?: boolean | undefined;
-              prefetch?: boolean | undefined;
-              locale?: string | false | undefined;
-              legacyBehavior?: boolean | undefined;
-              onMouseEnter?: React.MouseEventHandler<HTMLAnchorElement> | undefined;
-              onTouchStart?: React.TouchEventHandler<HTMLAnchorElement> | undefined;
-              onClick?: React.MouseEventHandler<HTMLAnchorElement> | undefined;
-            } & {
-              children?: React.ReactNode;
-            } & React.RefAttributes<HTMLAnchorElement>,
-            'locale'
-          > & {
-            locale: string;
-            localePrefix?: 'as-needed' | 'always' | 'never' | undefined;
-          },
-          'ref'
-        > &
-          React.RefAttributes<HTMLAnchorElement>,
-        'locale'
-      > & {
-        locale?: (typeof locales)[number] | undefined;
-      } & {
-        ref?: React.Ref<HTMLAnchorElement> | undefined;
-      },
-      'localePrefix'
-    > & {
-      ref?: React.Ref<HTMLAnchorElement> | undefined;
-    }
-  ) => React.ReactElement;
-  // eslint-disable-next-line no-unused-vars
-  redirect: (pathname: string, type?: RedirectType) => never;
-  usePathname: () => string;
-  useRouter: typeof useNextRouter;
-} = createSharedPathnamesNavigation({ locales, localePrefix });
+const navigation = createSharedPathnamesNavigation({ locales, localePrefix });
 
 export const { Link, redirect, usePathname, useRouter } = navigation;
