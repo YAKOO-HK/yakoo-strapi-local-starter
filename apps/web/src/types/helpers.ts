1d197ec767fc3cf299eb96caa0d9f8750f4989bd
HM Ku <hmku@yakoo.com.hk>
2024-09-05 15:34:57 +0800

    update dependencies (target=minor)

--- apps/web/src/types/helpers.ts
+++ apps/web/src/types/helpers.ts
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+// import { forwardRef } from 'react';
 
 export type UnwrapArray<T> = T extends Array<infer U> ? U : T;
 
@@ -9,9 +9,9 @@ export type Prettify<T> = {
 } & {};
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-export function fixedForwardRef<T, P = {}>(
-  render: (props: P, ref: React.Ref<T>) => React.ReactNode
-): (props: P & React.RefAttributes<T>) => React.ReactNode {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return forwardRef(render) as any;
-}
+// export function fixedForwardRef<T, P = {}>(
+//   render: (props: P, ref: React.Ref<T>) => React.ReactNode
+// ): (props: P & React.RefAttributes<T>) => React.ReactNode {
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   return forwardRef(render) as any;
+// }
