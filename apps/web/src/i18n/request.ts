02be33d21a219564103520e1040d0ba39d33a94d
Ming <hmku@yakoo.com.hk>
2025-01-28 13:43:44 +0800

    chore: update dependencies

--- apps/web/src/i18n/request.ts
+++ apps/web/src/i18n/request.ts
@@ -10,6 +10,7 @@ export default getRequestConfig(async ({ requestLocale }) => {
     locale = routing.defaultLocale;
   }
   return {
+    locale,
     messages: (await import(`../../messages/${locale}.json`)).default,
   };
 });
