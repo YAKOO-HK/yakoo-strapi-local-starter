da0e2b65d591113e8f0aaf4c7cbf655c95218aa3
Ming <hmku@yakoo.com.hk>
2025-03-03 18:17:04 +0800

    update typesense to v2, remove aws bedrock as provider

--- apps/web/src/lib/typesense.ts
+++ apps/web/src/lib/typesense.ts
@@ -1,9 +1,6 @@
 import { mkdirSync } from 'node:fs';
 import { join } from 'node:path';
 import { ChatCloudflareWorkersAI, CloudflareWorkersAI } from '@langchain/cloudflare';
-import { BedrockChat } from '@langchain/community/chat_models/bedrock';
-import { BedrockEmbeddings } from '@langchain/community/embeddings/bedrock';
-import { Bedrock } from '@langchain/community/llms/bedrock';
 import { Typesense, TypesenseConfig } from '@langchain/community/vectorstores/typesense';
 import { Document } from '@langchain/core/documents';
 import { ChatOpenAI, OpenAI, OpenAIEmbeddings } from '@langchain/openai';
@@ -11,63 +8,26 @@ import { CacheBackedEmbeddings } from 'langchain/embeddings/cache_backed';
 import { LocalFileStore } from 'langchain/storage/file_system';
 import { Client } from 'typesense';
 import { env } from '@/env';
-import { CloudflareWorkersAIEmbeddings } from '@/lib/langchain/cloudflare/embeddings';
 
 export function getEmbedding() {
-  if (env.TYPESENSE_EMBEDDINGS_PROVIDER === 'openai') {
-    mkdirSync(join(env.TYPESENSE_EMBEDDINGS_CACHE_PATH, 'openai'), { recursive: true });
-    return CacheBackedEmbeddings.fromBytesStore(
-      new OpenAIEmbeddings({
-        openAIApiKey: env.OPENAI_API_KEY,
-        modelName: 'text-embedding-3-small',
-      }),
-      new LocalFileStore({ rootPath: join(env.TYPESENSE_EMBEDDINGS_CACHE_PATH, 'openai') }),
-      {
-        namespace: 'text-embedding-3-small',
-      }
-    );
-  } else if (env.TYPESENSE_EMBEDDINGS_PROVIDER === 'cloudflare') {
-    mkdirSync(join(env.TYPESENSE_EMBEDDINGS_CACHE_PATH, 'cloudflare'), { recursive: true });
-    return CacheBackedEmbeddings.fromBytesStore(
-      new CloudflareWorkersAIEmbeddings({
-        cloudflareAccountId: env.CLOUDFLARE_ACCOUNT_ID,
-        cloudflareApiToken: env.CLOUDFLARE_API_TOKEN,
-        modelName: '@cf/baai/bge-large-en-v1.5',
-      }),
-      new LocalFileStore({ rootPath: join(env.TYPESENSE_EMBEDDINGS_CACHE_PATH, 'cloudflare') }),
-      {
-        namespace: 'bge-large-en-v1.5',
-      }
-    );
-  }
-  // defaults to bedrock
-  mkdirSync(join(env.TYPESENSE_EMBEDDINGS_CACHE_PATH, 'bedrock'), { recursive: true });
+  mkdirSync(join(env.TYPESENSE_EMBEDDINGS_CACHE_PATH, 'openai'), { recursive: true });
   return CacheBackedEmbeddings.fromBytesStore(
-    new BedrockEmbeddings({
-      region: env.AWS_REGION,
-      credentials: {
-        accessKeyId: env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+    new OpenAIEmbeddings({
+      openAIApiKey: env.OPENAI_API_KEY,
+      modelName: 'text-embedding-3-small',
+      configuration: {
+        baseURL: env.OPENAI_BASE_URL,
       },
-      model: 'amazon.titan-embed-text-v1', // Default value
     }),
-    new LocalFileStore({ rootPath: join(env.TYPESENSE_EMBEDDINGS_CACHE_PATH, 'bedrock') }),
+    new LocalFileStore({ rootPath: join(env.TYPESENSE_EMBEDDINGS_CACHE_PATH, 'openai') }),
     {
-      namespace: 'amazon.titan-embed-text-v1',
+      namespace: 'text-embedding-3-small',
     }
   );
 }
 
 export function getLLM({ streaming = true }) {
-  if (env.TYPESENSE_LLM_PROVIDER === 'openai') {
-    return new OpenAI({
-      modelName: 'gpt-3.5-turbo-0125',
-      temperature: 0.8,
-      maxTokens: 512,
-      streaming,
-      openAIApiKey: env.OPENAI_API_KEY, // In Node.js defaults to process.env.OPENAI_API_KEY
-    });
-  } else if (env.TYPESENSE_LLM_PROVIDER === 'cloudflare') {
+  if (env.TYPESENSE_LLM_PROVIDER === 'cloudflare') {
     return new CloudflareWorkersAI({
       model: '@cf/qwen/qwen1.5-7b-chat-awq',
       streaming,
@@ -75,31 +35,21 @@ export function getLLM({ streaming = true }) {
       cloudflareApiToken: env.CLOUDFLARE_API_TOKEN,
     });
   }
-  // defaults to bedrock
-  return new Bedrock({
-    // model: 'meta.llama2-13b-chat-v1',
-    model: 'anthropic.claude-3-haiku-20240307-v1:0',
-    region: env.AWS_REGION,
+  // defaults to openai
+  return new OpenAI({
+    modelName: 'gpt-4o-mini',
     temperature: 0.8,
     maxTokens: 512,
     streaming,
-    credentials: {
-      accessKeyId: env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+    openAIApiKey: env.OPENAI_API_KEY,
+    configuration: {
+      baseURL: env.OPENAI_BASE_URL,
     },
   });
 }
 
 export function getChatModel({ streaming = true, maxTokens = 512, temperature = 0.8 }) {
-  if (env.TYPESENSE_LLM_PROVIDER === 'openai') {
-    return new ChatOpenAI({
-      modelName: 'gpt-3.5-turbo-0125',
-      temperature,
-      maxTokens,
-      streaming,
-      openAIApiKey: env.OPENAI_API_KEY, // In Node.js defaults to process.env.OPENAI_API_KEY
-    });
-  } else if (env.TYPESENSE_LLM_PROVIDER === 'cloudflare') {
+  if (env.TYPESENSE_LLM_PROVIDER === 'cloudflare') {
     return new ChatCloudflareWorkersAI({
       model: '@cf/qwen/qwen1.5-7b-chat-awq',
       streaming,
@@ -107,19 +57,14 @@ export function getChatModel({ streaming = true, maxTokens = 512, temperature = 
       cloudflareApiToken: env.CLOUDFLARE_API_TOKEN,
     });
   }
-
-  // defaults to bedrock
-  return new BedrockChat({
-    // model: 'meta.llama2-13b-chat-v1',
-    model: 'anthropic.claude-3-haiku-20240307-v1:0',
-    // model: 'mistral.mixtral-8x7b-instruct-v0:1',
-    region: env.AWS_REGION,
+  return new ChatOpenAI({
+    modelName: 'gpt-4o-mini',
     temperature,
     maxTokens,
     streaming,
-    credentials: {
-      accessKeyId: env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+    openAIApiKey: env.OPENAI_API_KEY,
+    configuration: {
+      baseURL: env.OPENAI_BASE_URL,
     },
   });
 }
